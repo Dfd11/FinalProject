7d5de439f62c89062bee48db1c43e0f49802cf53
Dfd11 <a01209414@tec.mx>
2022-01-30 22:35:47 -0600

    Added buttons to navigation simple

--- server.js
+++ server.js
@@ -18,40 +18,57 @@ app.listen(PORT, function() {
 
 app.get("/", function(req,res){
     //res.sendFile(path.join(__dirname,"home.html"));
-    res.send("This is the 'HOME PeGE'")
+    res.send(`This is the HOME PAGE 
+    <form action="/login" method="GET"><button>LOGIN</button></form>
+    <form action="/copyright" method="GET"><button>COPYRIGHT</button></form>
+    <form action="/about_us" method="GET"><button>ABOUT US</button></form> 
+    <form action="/contact" method="GET"><button>CONTACT</button></form>`)
 })
 
 app.get("/login", function(req,res){
     //res.sendFile(path.join(__dirname,"login.html"));
-    res.send("This is the 'LOGIN PAGE'")
+    res.send(`This is the LOGIN PAGE 
+    <form action="/" method="GET"><button>ROOT</button></form> 
+    <form action="/register" method="GET"><button>REGISTER</button></form>
+    <form action="/user/test" method="GET"><button>LOGIN TEST</button></form>`)
 })
 
 app.get("/register", function(req,res){
     //res.sendFile(path.join(__dirname,"register.html"));
-    res.send("This is the 'REGISTER PAGE'")
+    res.send(`This is the REGISTER PAGE 
+    <form action="/user/test" method="GET"><button>REGISTER TEST</button></form>
+    <form action="/" method="GET"><button>ROOT</button></form>`)
 })
 
-app.get("/:user", function(req,res){
+app.get("/user/:user", function(req,res){
     //res.sendFile(path.join(__dirname,"home.html"));
-    res.send(`This is the '${req.params.user} HOME PAGE'`)
+    res.send(`This is the '${req.params.user} HOME PAGE 
+    <form action="/${req.params.user}/devices" method="GET"><button>DEVICES</button></form>`)
 })
 
 app.get("/:user/devices", function(req,res){
  //   res.sendFile(path.join(__dirname,"home.html"));
-    res.send("This is the 'USERS DEVICES PAGE'")
+    res.send(`This is the 'USERS DEVICES PAGE' 
+    <form action="/user/${req.params.user}" method="GET"><button>USER HOME</button></form>`)
 })
 
 app.get("/copyright", function(req,res){
    // res.sendFile(path.join(__dirname,"home.html"));
-   res.send("This is the 'COPYRIGHT PAGE'")
+   res.send(`This is the 'COPYRIGHT PAGE' 
+   <form action="/" method="GET"><button>ROOT</button></form>
+   <form action="/user/test" method="GET"><button>USER HOME</button></form>`)
 })
 
 app.get("/about_us", function(req,res){
     //res.sendFile(path.join(__dirname,"home.html"));
-    res.send("This is the 'ABOUT US PAGE'")
+    res.send(`This is the 'ABOUT US PAGE'
+    <form action="/" method="GET"><button>ROOT</button></form>
+    <form action="/user/test" method="GET"><button>USER HOME</button></form>`)
 })
 
 app.get("/contact", function(req,res){
     //res.sendFile(path.join(__dirname,"home.html"));
-    res.send("This is the 'CONTACT PAGE'")
+    res.send(`This is the 'CONTACT PAGE'
+    <form action="/" method="GET"><button>ROOT</button></form>
+    <form action="/user/test" method="GET"><button>USER HOME</button></form>`)
 })
